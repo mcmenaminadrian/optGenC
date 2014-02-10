60dd816988768b99e932a04eea7e87f5d1b66a4d
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-02-10 23:46:12 +0000

    won't parse

--- optgenc.c
+++ optgenc.c
@@ -24,10 +24,10 @@ int main(int argc, char *argv[])
 {
 	FILE* inXML;
 	ssize_t read;
-	char* line = NULL;
+	char* line;
 	size_t len = 0;
 
-	XML_Parser p_ctrl = XML_ParserCreate(NULL);
+	XML_Parser p_ctrl = XML_ParserCreate("UTF-8");
 	if (!p_ctrl) {
 		fprintf(stderr, "Could not create parser\n");
 		exit(-1);
@@ -42,12 +42,20 @@ int main(int argc, char *argv[])
 	}
 
 	while ((read = getline(&line, &len, inXML)) != -1) {
-		XML_Parse(p_ctrl, line, len, 0);
+		printf("Line is %s", line);
+		enum XML_Status status = XML_Parse(p_ctrl, line, len, 0);
+		if (status == 0) {
+			enum XML_Error errcde = XML_GetErrorCode(p_ctrl);
+			printf("ERROR: %s\n", XML_ErrorString(errcde));
+			printf("Error at column number %lu\n", XML_GetCurrentColumnNumber(p_ctrl));
+			printf("Error at line number %lu\n", XML_GetCurrentLineNumber(p_ctrl));
+		}
 		free(line);
 		line = NULL;
+		len = 0;
 	}
 
 	XML_ParserFree(p_ctrl);
-
-	return 1;
+	fclose(inXML);
+	return 0;
 }
